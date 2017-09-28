548c6b4b5eca971c0bcad5394f4a61a75c325601
Nicolas Bortolotti <nbortolotti@gmail.com>
2017-09-28 17:03:25 +0200

    added travis support, create a generic template for unittest. added requeriments.txt file support. Changed the structure of the main method.

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -3,23 +3,27 @@ __author__ = 'nbortolotti'
 import tensorflow as tf
 import argparse
 
-if __name__ == '__main__':
+
+def main():
     pa = argparse.ArgumentParser(description='example to use arithmetic_operators')
-    pa.add_argument('--operator', dest='operator', required=True, help='add,subtract')
-    pa.add_argument('--value_a', dest='value_a', required=True, help='value a')
-    pa.add_argument('--value_b', dest='value_b', required=True, help='value b')
+    pa.add_argument('--operator', dest='operator', help='add,subtract', type=str, default='add')
+    pa.add_argument('--value_a', dest='value_a', help='value a', type=int, default=1)
+    pa.add_argument('--value_b', dest='value_b', help='value b', type=int, default=1)
+
+    args = pa.parse_args()
+
+    a = tf.constant(int(args.value_a))
+    b = tf.constant(int(args.value_b))
+
+    operation = None
+    if args.operator == 'add':
+        operation = tf.add(a, b)
+    elif args.operator == 'subtract':
+        operation = tf.subtract(a, b)
+
+    with tf.Session() as ses:
+        print ses.run(operation)
 
 
-args = pa.parse_args()
-
-a = tf.constant(int(args.value_a))
-b = tf.constant(int(args.value_b))
-
-operation = None
-if args.operator == 'add':
-    operation = tf.add(a, b)
-elif args.operator == 'subtract':
-    operation = tf.subtract(a, b)
-
-with tf.Session() as ses:
-    print ses.run(operation)
+if __name__ == '__main__':
+    main()
---
+++ test/arithmetic_operators_test.py
@@ -0,0 +1,20 @@
+__author__ = 'nbortolotti'
+
+# import unittest
+#
+#
+# class MainTestCase(unittest.TestCase):
+#     def general_test(self):
+#         pass
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+from unittest import TestCase
+
+
+class GeneralMain(TestCase):
+    def test_main(self):
+        pass
