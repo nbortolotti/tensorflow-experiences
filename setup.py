9d710b0a0a443bfbcdd61ffcee542b55ecaa4c71
Nicolas Bortolotti <nbortolotti@gmail.com>
2017-09-28 22:10:08 +0200

    adding setup.py new file config.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='arithmetic_operators',
+      version='0.1',
+      description='arithmetic operator',
+      url='https://github.com/nbortolotti/tensorflow-code-experiences',
+      author='Nicolas Bortolotti',
+      author_email='nbortolotti@gmail.com',
+      license='Apache-2.0',
+      packages=['arithmetic_operators'],
+      zip_safe=False)
